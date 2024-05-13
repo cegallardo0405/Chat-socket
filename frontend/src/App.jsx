1a4a824e3d26814ada870d3c80d945c80cfc6378
Carlos López Gallardo <124450022+cegallardo0405@users.noreply.github.com>
2024-05-13 12:55:07 -0500

    Fixing Bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
+import '/BackStyle.css';
 
 // const socket = io("http://localhost:3001");
 const socket = io("/");
@@ -32,16 +33,15 @@ export default function App() {
   };
 
   return (
-    <div className="h-screen bg-zinc-800 text-white flex items-center justify-center">
-      <form onSubmit={handleSubmit} className="bg-zinc-900 p-10 rounded-md w-1/2">
+    <div className="h-screen bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 text-white flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="blurred-box p-10 rounded-xl w-1/2">
         <h1 className="text-2xl font-bold my-2">Chat</h1>
-
 
         <ul className="h-80 overflow-y-auto flex flex-col-reverse rounded-md">
           {messages.map((message, index) => (
               <li
                   key={index}
-                  className={`my-2 p-2 table text-sm rounded-md ${message.from === "Me" ? "bg-sky-700 ml-auto" : "bg-black"
+                  className={`my-2 p-2 block text-sm rounded-md ${message.from === "Me" ? "bg-sky-700 ml-auto" : "bg-black mr-auto"
                   }`}
               >
               <span className={'block text-xs text-slate-300'}>
@@ -50,7 +50,6 @@ export default function App() {
                 <span className={'text-sm'}>
                 {message.body}
               </span>
-
               </li>
           ))}
         </ul>
@@ -60,7 +59,7 @@ export default function App() {
             type="text"
             placeholder="Write your message..."
             onChange={(e) => setMessage(e.target.value)}
-            className="border-2 border-zinc-500 p-2 w-full text-black rounded-md"
+            className="border-2 p-2 w-full text-white rounded-md blurred-box"
             value={message}
             autoFocus
         />
